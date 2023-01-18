d0f0b7071df87ead9e01de73ef4a8084d242b721
Saswata Banerjee <azure.saswata@gmail.com>
2023-01-18 17:59:12 +0530

    Templates and renders

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8c381p(!rh%_vqcg%h-*f4_xty3a3cfh&n+5fk0(3q$epgw3i5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
