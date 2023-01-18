266e3d257240841fe49a7897d202cfaba3eb06dc
Saswata Banerjee <azure.saswata@gmail.com>
2023-01-18 19:15:05 +0530

    add rolls app - similar to polls app with VIEWs

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'rolls.apps.RollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- djangoproject/urls.py
+++ djangoproject/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('rolls/', include('rolls.urls')),
     # path('', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
